be88d64ef1f62b983bf04c3a5ab3c8f137811070
Gachi_Dima <dimitriusdima4@gmail.com>
2026-09-21 22:04:36 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,15 @@
 from unittest.mock import MagicMock, patch
 from app.main import can_access_google_page
+import pytest
 
+
+@pytest.mark.parametrize("url", ["https://google.com",
+                                 "https://www.google.com"])
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page_accessible(
-    mock_valid_google_url: MagicMock, mock_has_internet_connection: MagicMock, url: str
+    mock_valid_google_url: MagicMock,
+    mock_has_internet_connection: MagicMock, url: str
 ) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
@@ -12,11 +17,13 @@ def test_can_access_google_page_accessible(
     assert result == "Accessible"
 
 
-
+@pytest.mark.parametrize("url", ["https://google.com",
+                                 "https://www.google.com"])
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page_no_internet(
-    mock_has_internet_connection: MagicMock, mock_valid_google_url: MagicMock, url: str
+    mock_has_internet_connection: MagicMock,
+    mock_valid_google_url: MagicMock, url: str
 ) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
@@ -24,10 +31,13 @@ def test_can_access_google_page_no_internet(
     assert result == "Not accessible"
 
 
+@pytest.mark.parametrize("url", ["https://google.com",
+                                 "https://www.google.com"])
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page_invalid_url(
-    mock_has_internet_connection: MagicMock, mock_valid_google_url: MagicMock, url: str
+    mock_has_internet_connection: MagicMock,
+    mock_valid_google_url: MagicMock, url: str
 ) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
@@ -35,10 +45,13 @@ def test_can_access_google_page_invalid_url(
     assert result == "Not accessible"
 
 
+@pytest.mark.parametrize("url", ["https://google.com",
+                                 "https://www.google.com"])
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page_invalid_url_and_no_internet(
-    mock_has_internet_connection: MagicMock, mock_valid_google_url: MagicMock, url: str
+    mock_has_internet_connection: MagicMock,
+    mock_valid_google_url: MagicMock, url: str
 ) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = False
